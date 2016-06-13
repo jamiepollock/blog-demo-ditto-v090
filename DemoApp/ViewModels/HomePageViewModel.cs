34a8924900f8e43c1ca495de5b61dfd2d5c6ac61
Jamie Pollock <jamie.pollock@connect-group.com>
2016-06-13 17:11:47 +0100

    Step 2: Provide alt property fallbacks
    This step adds in two additional fallbacks in case the default property is missing.
    
    * The Meta Title from the SEO: Meta Data tab
    * The Name , which is the name of the Node in Umbraco
    
    No other changes have been added other than the Order property which is used to ensure attributes run in order.

--- DemoApp/ViewModels/HomePageViewModel.cs
+++ DemoApp/ViewModels/HomePageViewModel.cs
@@ -4,7 +4,9 @@ namespace DemoApp.ViewModels
 {
     public class HomePageViewModel
     {
-        [UmbracoProperty("content_title")]
+        [UmbracoProperty("content_title", Order = 0)]
+        [AltUmbracoProperty("seo_metaTitle", Order = 1)]
+        [AltUmbracoProperty("Name", Order = 2)]
         public string Title { get; set; }
     }
 }
