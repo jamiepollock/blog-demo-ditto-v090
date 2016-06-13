a5831750bd2deb8d6c4ccf81bc50ef1ce042c0a0
Jamie Pollock <jamie.pollock@connect-group.com>
2016-06-13 17:09:06 +0100

    Step 1: Add in ViewModel & Controller
    This steps adds:
    
    * A ViewModel with a basic UmbracoProperty to bind it to the content_title property
    * HomePageController to bind the ViewModel to current page
    * Updated the view to display the strongly typed properties from our new ViewModel

---
+++ DemoApp/Controllers/HomePageController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using DemoApp.ViewModels;
+using Our.Umbraco.Ditto;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace DemoApp.Controllers
+{
+    public class HomePageController : RenderMvcController
+    {
+        public ActionResult HomePage(RenderModel model)
+        {
+            var viewModel = model.As<HomePageViewModel>();
+
+            return View(viewModel);
+        }
+
+    }
+}
---
+++ DemoApp/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Our.Umbraco.Ditto;
+
+namespace DemoApp.ViewModels
+{
+    public class HomePageViewModel
+    {
+        [UmbracoProperty("content_title")]
+        public string Title { get; set; }
+    }
+}
